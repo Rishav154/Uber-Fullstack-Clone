9e534cc2e15eed98239c5066fe6e457c77aaa494
Rishav154 <rishav15045@gmail.com>
2025-03-05 22:50:05 +0530

    ..

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import UserLogin from './pages/UserLogin'
+import UserSignup from './pages/UserSignup'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignup from './pages/CaptainSignup'
+import Home from './pages/Home'
+
+const App = () => {
+
+    return (
+        <div>
+            <Routes>
+                <Route path='/login' element={<UserLogin />} />
+                <Route path='/signup' element={<UserSignup />} />
+                <Route path='/captain-login' element={<CaptainLogin />} />
+                <Route path='/captain-signup' element={<CaptainSignup />} />
+                <Route path='/' element={<Home />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App
